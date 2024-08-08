a325f9126f525f3e38e9a54b8a0242eb383a9e3f
Marinna Souza <marinna.luiza54@gmail.com>
2024-08-08 14:13:17 -0300

    Listagem com o GET e Exclusão com o DELETE.

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,40 +1,40 @@
-import{useRef} from 'react'
-import Produtos from "./Produtos"
+import { useRef } from 'react';
+import Produtos from "./Produtos";
 
 export default function Login(){
     const user = useRef();
     const password = useRef();
 
     const getUser = sessionStorage.getItem('userData');
-    const getPassaword = sessionStorage.getItem('senhaData');
+    const getToken = sessionStorage.getItem('senhaData');
 
-    const handleSubmit = () =>{
-    
-        if(user.current.value === 'Admin' && getPassaword.current.value ==='123456'){
-            
+    const handleSubmit = (e) =>{
+        e.preventDefault(); // Previne o comportamento padrão de submit
+
+        if(user.current.value === 'Admin' && password.current.value === '160880'){
             //Autenticação
-            let token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+            let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
             sessionStorage.setItem('userData', 'Admin');
             sessionStorage.setItem('senhaData', token);
+            window.location.reload(); // Recarrega a página para refletir o login
         }else{
-            alert("Usuario ou senha invalidos")
+            alert("Usuário ou senha inválidos");
         }
     }
 
     return(
-    <section>
-        {getUser && getPassaword ? (
-            <Produtos/>
-
-        ):(
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="usuario">Usuario:</label>
-                <input type="text" ref={user}/>
-                <label htmlFor="password">Senha</label>
-                <input type="password" ref={password}/>
-                <input type="submit" value="Login" />
-            </form>
-        )}
-    </section>
-    )
+        <section>
+            {getUser && getToken ? (
+                <Produtos/>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="usuario">Usuário:</label>
+                    <input type="text" ref={user} />
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" ref={password} />
+                    <input type="submit" value="Login" />
+                </form>
+            )}
+        </section>
+    );
 }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,59 @@
+import { useState, useEffect } from "react"
+
 export default function Produtos(){
+    const [produtosApi, setProdutosApi] = useState([])
+
+    useEffect(
+        () => {
+            fetch('http://localhost:5000/produtos')
+            .then((resp) => resp.json())
+            .then((resp) => setProdutosApi(resp))
+            .catch((error) => console.log(error))
+        },[]
+    )
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/produtos/${id}`, {method: "delete"})
+        .then(()=> (window.location = '/produtos'))
+        .catch((error) => console.log(error))
+    }
     return(
         <section>
-            <h2>Produtos</h2>
+            <button>Cadastrar Livro</button>
+            <h1>Lista de livros</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Autor(a)</th>
+                        <th>Editora</th>
+                        <th>Descrição</th>
+                        <th>Preço</th>
+                        <th>Editar / Excluir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtosApi.map((prod) => (
+                        <tr key={prod.id}>
+                            <td>{prod.nome}</td>
+                            <td>{prod.autor}</td>
+                            <td>{prod.editora}</td>
+                            <td>{prod.desc}</td>
+                            <td>{prod.preco}</td>
+                            <td>
+                                <button onClick={handleDelete.bind(this, prod.id)}>Deletar</button>
+                            </td>
+
+                        </tr>
+                    ))
+                    }
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="6" >Listar de Livros em promoção</td>
+                    </tr>
+                </tfoot>
+            </table>
         </section>
     )
 }
